d78fef6927af4132c008fd97bbe8ed87afc7e27b
Max Vdovin <maxpostupaet@gmail.com>
2023-09-28 10:00:59 +0300

    Optimized implementation of bubbleSort

--- src/main/java/com/vdovin/basic_algoritms/sorting/BubbleSort.java
+++ src/main/java/com/vdovin/basic_algoritms/sorting/BubbleSort.java
@@ -1,16 +1,24 @@
 package com.vdovin.basic_algoritms.sorting;
 
 public class BubbleSort {
-    //Time: O(n^2)
+    //Time: O(n * ((n + 1) / 2 ))
     //Space: O(1)
     public static int bubbleSort(int[] nums) {
         int countIteration = 0;
+        boolean swapped;
         for (int i = 0; i < nums.length; i++) {
+            swapped = false;
             for (int j = 0; j < nums.length - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     swap(nums, j, j + 1);
+                    swapped = true;
                 }
                 countIteration++;
+
+                if (!swapped) {
+                    break; //Если прошли по всему массиву и не произвели
+                           // ни одной перестановки, то массив отсортирован
+                }
             }
         }
         return countIteration;
@@ -18,7 +26,7 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] nums1 = new int[]{6, 5, 4, 3, 2, 1};
-        int[] nums2= new int[]{1, 2, 3, 4, 5, 6};
+        int[] nums2 = new int[]{1, 2, 3, 4, 5, 6};
         int count = bubbleSort(nums2);
         printArray(nums2);
         System.out.println("Count iteration: " + count);
