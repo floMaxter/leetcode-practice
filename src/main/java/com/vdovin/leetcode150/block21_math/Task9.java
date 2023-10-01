bf75bdfbc4e9fb5e7defa67a6972864f6fcbc67e
Max Vdovin <maxpostupaet@gmail.com>
2023-10-01 23:02:19 +0300

    Added task 9. Palindrome Number, solving with string (bed solving)

---
+++ src/main/java/com/vdovin/leetcode150/block21_math/Task9.java
@@ -0,0 +1,15 @@
+package com.vdovin.leetcode150.block21_math;
+
+public class Task9 {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int l = 0, r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
