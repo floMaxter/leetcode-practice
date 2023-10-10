31126141b1e84312d8d4a93cf3bf0f76ae469ad8
Max Vdovin <maxpostupaet@gmail.com>
2023-10-10 11:41:45 +0300

    Added task 104. Maximum Depth of Binary Tree. Iterative DFS soluiton.

--- src/main/java/com/vdovin/leetcode150/block9_binaryTreeGeneral/Task104.java
+++ src/main/java/com/vdovin/leetcode150/block9_binaryTreeGeneral/Task104.java
@@ -5,30 +5,31 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Task104 {
-    //Iterative BFS
+    //Iterative DFS
     //Time: O(n)
     //Space: O(n)
     public static int maxDepth(TreeNode root) {
         if (root == null) return 0;
 
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        int count = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size > 0) {
-                TreeNode node = queue.poll();
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-                size--;
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> value = new Stack<>();
+        stack.push(root);
+        value.push(1);
+        int max = 0;
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int temp = value.pop();
+            max = Math.max(temp, max);
+            if (node.right != null) {
+                stack.push(node.right);
+                value.push(temp+1);
             }
-            count++;
+            if (node.left != null) {
+                stack.push(node.left);
+                value.push(temp+1);
+            }
         }
-        return count;
+        return max;
     }
 
 
