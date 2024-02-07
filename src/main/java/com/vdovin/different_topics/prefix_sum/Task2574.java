31dc9b4d128d3f4fb514cb85161622fa74ece724
Max Vdovin <maxpostupaet@gmail.com>
2024-02-07 15:16:14 +0300

    Add Task 2574. Left and Right sum Differences with Time Complexity O(n)

--- src/main/java/com/vdovin/different_topics/prefix_sum/Task2574.java
+++ src/main/java/com/vdovin/different_topics/prefix_sum/Task2574.java
@@ -3,24 +3,20 @@ package com.vdovin.different_topics.prefix_sum;
 import java.util.Arrays;
 
 public class Task2574 {
-    //Time: O(n^2)
-    //Space: O(n)
+    //Time: O(n)
+    //Space: O(1)
     public static int[] leftRightDifference(int[] nums) {
-        int[] res = new int[nums.length];
+        int leftSum = 0, rightSum = 0;
+        for (int num : nums) {
+            rightSum += num;
+        }
         for (int i = 0; i < nums.length; i++) {
-            int leftSum = getPrefixSum(nums, 0, i);
-            int rightSum = getPrefixSum(nums, i + 1, nums.length);
-            res[i] = Math.abs(leftSum - rightSum);
+            int val = nums[i];
+            rightSum -= val;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum += val;
         }
-        return res;
-    }
-
-    public static int getPrefixSum(int[] nums, int l, int r) {
-        int sum = 0;
-        for (int i = l; i < r; i++) {
-            sum += nums[i];
-        }
-        return sum;
+        return nums;
     }
 
     public static void main(String[] args) {
